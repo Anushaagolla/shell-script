f300eac45b559080478d05de5b5a9ad9d38f390b
Anushaagolla <anuanusha685@gmail.com>
2023-12-01 18:25:53 -0500

    functions

---
+++ 07-functions.sh
@@ -0,0 +1,20 @@
+VALIDATE {
+    if [ $2 -ne 0 ]
+    then
+        echo "$1 installation is not successful"
+    else
+        echo "$1 installation is successful"
+    fi
+}
+
+VALIDATE  $1 $? "
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ] 
+then
+    echo "ERROR : run this command with root access"
+fi
+
+Packagename=$1
+yum install $Packagename -y
