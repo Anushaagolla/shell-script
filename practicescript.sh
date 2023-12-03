5e5827a1d488d0c4b23445c914b932a3c30efd37
Anushaagolla <anuanusha685@gmail.com>
2023-12-03 13:52:10 -0500

    practice

--- practicescript.sh
+++ practicescript.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-user1=$1
-user2=$2
+# user1=$1
+# user2=$2
 
-echo "$user1 :hi, Good Morning"
-echo "$user2:good morning"
+# echo "$user1 :hi, Good Morning"
+# echo "$user2:good morning"
 
-echo "enter username"
-read -s $USERNAME
+# echo "enter username"
+# read -s $USERNAME
+
+# echo "enter password"
+# read $PASSWORD
+
+num1=$1
+num2=$2
+sum=($(num1+num2))
+echo "sum of the two numbers is :$sum"
