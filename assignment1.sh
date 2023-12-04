0ece46bff10e445a835eea473f32c4816fe98a87
Anushaagolla <anuanusha685@gmail.com>
2023-12-04 17:45:15 -0500

    reading a file

--- assignment1.sh
+++ assignment1.sh
@@ -4,6 +4,6 @@ file=/c/users/anuan/test.txt
 while read line
 do
     echo "$line"
-    cat $file | xargs printf “%s\n” | sort | uniq -c | sort -nr | awk ‘{print $2,$1}’
+    cat $file | xargs printf “%s\n” | sort | uniq -c | sort -nr | awk {print $2,$1}
 
 done < $file
