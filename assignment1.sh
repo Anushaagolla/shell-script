bad07bc126c69add2e0acd65804e04afe21f9860
Anushaagolla <anuanusha685@gmail.com>
2023-12-05 10:17:56 -0500

    replace word

--- assignment1.sh
+++ assignment1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 file=/c/users/anuan/test.txt
 
-#cat $file | tr ' ' '\n' | sort | uniq -c
+cat $file | tr ' ' '\n' | sort | uniq -c
 
-while read line
-do
-    echo "$line" | awk '{print $N}' | sort -d | uniq -c
-done < $file
+# while read line
+# do
+#     echo "$line" | awk '{print $N}' | sort -d | uniq -c
+# done < $file
---
+++ assignment2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+file=/c/users/anuan/test.txt
+
+cat $file | tr 'this' 'that' 
