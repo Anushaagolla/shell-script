c5cedb162d28cd3b85785269c5003a882eadda23
Anushaagolla <anuanusha685@gmail.com>
2023-12-05 09:50:39 -0500

    reading a file

--- assignment1.sh
+++ assignment1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 file=/c/users/anuan/test.txt
 
-while read line
-do
-    echo "$line" | tr ' ' '\n' | sort | sort -d | uniq -c
-done < $file
+cat $file | tr ' ' '\n' | sort | sort -d | uniq -c
+
+# while read line
+# do
+#     echo "$line" | tr ' ' '\n' | sort | sort -d | uniq -c
+# done < $file
