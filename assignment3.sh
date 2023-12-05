d87f7e2d5f26e2a13fc1a24e2e4116bdfd48b5fb
Anushaagolla <anuanusha685@gmail.com>
2023-12-05 11:00:15 -0500

    palindrome

--- assignment3.sh
+++ assignment3.sh
@@ -3,14 +3,14 @@
 echo "enter your input"
 read $input
 
-len=$(#input)
-for ((i=$len-1; i>=0; i--))
+len=${#input}
+for (( i=$len; i>=0; i-- ))
 do
-    reverse=$reverse${input:$i:1}
+    reverse="$reverse${input:$i:1}"
 done
 if [ $input == $reverse]
 then
-    echo "given input is palindrome"
+    echo "$input is palindrome"
 else
-    echo "given input is not palindrome"
+    echo "$input is not palindrome"
 fi
