76e976bf24c74f0babd8a70c6ca7e3555fe094e0
Anushaagolla <anuanusha685@gmail.com>
2023-12-05 10:57:11 -0500

    palindrome

---
+++ assignment3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "enter your input"
+read $input
+
+len=$(#input)
+for(i=$len; i>=0; i--)
+do
+    reverse=$reverse${input:$i:1}
+done
+if [ $input == $reverse]
+then
+    echo "given input is palindrome"
+else
+    echo "given input is not palindrome"
+fi
